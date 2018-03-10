bb57845da0bdaf3650cbb7770736a1010f63b023
BuildTools <unconfigured@null.spigotmc.org>
2018-03-09 19:16:41 -0800

    Added file iteratetion

--- dropboxSyncApi/index.php
+++ dropboxSyncApi/index.php
@@ -1,6 +1,15 @@
 <?php
 class DropboxV2Client
 {
-  
+  function traverse_files($root)
+  {
+    foreach(new DirectoryIterator($root) as $fileInfo)
+    {
+        if($fileInfo->isDir() && (!$fileInfo->isDot()))
+        {
+          // Compare with dropbox eqvuvelent
+        }
+    }
+  }
 }
 ?>
