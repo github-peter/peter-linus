83fbdcf684bbe0db3f9ac09102c698013ae24cb1
Linus_J <30862662+swessey@users.noreply.github.com>
2018-03-10 21:17:53 -0800

    Edit with Music & README file

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Peter &amp; Linus development files
 2. Api nyckel i en password input på hemsidan. Ska matcha med den giltiga
 3. Meddelande ifall det inte gick eller om det gick.
 4. Laddnings icon som visar ifall den håller på.
-5. Raportera actuell status & inte kunna klicka på knappen igen förrän den är klar
+5. Raportera actuell status & inte kunna klicka på knappen igen förrän den är klar. Och Lägga till så att musiken fortsätter när man går in på hemsidan tills synkninen är klar. Samt att musiken slutar när synkningen är klar
 6. Lätt sätt att synka filerna.
 
   Alternativ är att köra allt skriptat i terminalen med ett pogram som simulerar det istället för att ha det på en hemsida.
--- dropboxSyncApi/index.php
+++ dropboxSyncApi/index.php
@@ -284,73 +284,75 @@ if (/*API KEY length + API KEY must be valid & must be a api that is connected w
   </head>
   <body>
 
-    <!--
-    Loader with percentage for sync progress.
-  -->
+        <!--
+        Loader with percentage for sync progress.
+      -->
 
-      <script>
-    function play(){
-       var audio = document.getElementById("audio");
-       audio.play();
-                 }
-          //stop playing when done syncing
-      </script>
-      <script>
-      // progressbar.js@1.0.0 version is used
-      // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+          <script type="text/javascript">
 
-      var bar = new ProgressBar.Line(container, {
-        strokeWidth: 4,
-        easing: 'easeInOut',
-        duration: 1400,
-        color: '#FFEA82',
-        trailColor: '#eee',
-        trailWidth: 1,
-        svgStyle: {width: '100%', height: '100%'},
-        text: {
-          style: {
-            // Text color.
-            // Default: same as stroke color (options.color)
-            color: '#999',
-            position: 'absolute',
-            right: '0',
-            top: '30px',
-            padding: 0,
-            margin: 0,
-            transform: null
-          },
-          autoStyleContainer: false
-        },
-        from: {color: '#FFEA82'},
-        to: {color: '#ED6A5A'},
-        step: (state, bar) => {
-          bar.setText(Math.round(bar.value() * 100) + ' %');
-        }
-      });
+              function play(id)
+              {
+                // alert("id="+id);
+                var audio1 = document.getElementById("sync");
+                var audio2 = document.getElementById("syncHome");
+                var audio3 = document.getElementById("bothSync");
 
-      bar.animate(1.0);  // Number from 0.0 to 1.0
-      </script>
-      <style media="screen">
-        #container {
-          margin: 20px;
-          width: 400px;
-          height: 8px;
-          position: relative;
-        }
-      </style>
+                if (id == "sync")
+                {
+                  audio2.pause();
+                  audio3.pause();
+                  audio1.play();
+                  audio2.currentTime = 0;
+                  audio3.currentTime = 0;
 
-    <div id="container"></div>
+                }
+                else if (id == "syncHome")
+                {
+                  audio1.pause();
+                  audio3.pause();
+                  audio2.play();
+                  audio3.currentTime = 0;
+                  audio1.currentTime = 0;
 
-    <input type="button" value="PLAY"  onclick="play()">
-    <audio id="audio" src="EM.mp3" controls loop >
+                }
+                else if (id == "bothSync")
+                {
+                  audio1.pause();
+                  audio2.pause();
+                  audio3.play();
+                  audio2.currentTime = 0;
+                  audio1.currentTime = 0;
 
-    </audio>
+                }
+                else
+                {
+                  audio1.pause();
+                  audio2.pause();
+                  audio3.pause();
+                  audio2.currentTime = 0;
+                  audio1.currentTime = 0;
+                  audio3.currentTime = 0;
+                }
+              }
+          </script>
 
+        <audio id="sync" src="EM.mp3" loop >
 
-    <input type="password" name="password input" >
+        </audio>
+        <audio id="syncHome" src="EM.mp3" loop >
 
-    <button class="button" type="button" id="sync" name="sync">Sync with dopbox</button>
-    <button class="button" type="button" id="syncHome" name="syncHome">Sync Home</button>
-    <button class="button" type="button" id="bothSync" name="bothSync">Both</button>
+        </audio>
+        <audio id="bothSync" src="EM.mp3" loop >
+
+        </audio>
+
+        <center>
+        <input type="password" name="password input">
+        <br>
+          <button class="button" type="button" id="sync" name="sync" onclick="play('sync')">Sync with dopbox</button>
+          <button class="button" type="button" id="syncHome" name="syncHome" onclick="play('syncHome')">Sync Home</button>
+          <button class="button" type="button" id="bothSync" name="bothSync" onclick="play('bothSync')">Both</button>
+          <div id="container"></div>
+       </center>
   </body>
 </html>
