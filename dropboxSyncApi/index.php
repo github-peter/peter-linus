1c5241c3510a6ae0c0c1f4f227ba924e4edd3c29
Linus_J <30862662+swessey@users.noreply.github.com>
2018-03-12 17:31:34 -0700

    Comments

--- dropboxSyncApi/index.php
+++ dropboxSyncApi/index.php
@@ -272,7 +272,7 @@ class DropboxV2Client
 <?php
 if (/*API KEY length + API KEY must be valid & must be a api that is connected with "peter's" Dropbox so no one else can steal and download the files to their  */) {
     // Allowing to press the buttons to sync to dropbox
-    
+    // Add header ./ so yoi can't submit Form again
   } else {
     // Button's continue to be locked
   }
